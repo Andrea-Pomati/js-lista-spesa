e679acef47c1234262f39d0e8b84c0b913a8fda6
Andrea-Pomati <pomati.andrea@gmail.com>
2023-03-01 14:55:23 +0100

    feat: const lista spesa

---
+++ js/script.js
@@ -0,0 +1,23 @@
+//Esercizio di oggi: Lista della spesa con ciclo while
+/* Consegna:
+Data una lista della spesa (memorizzata in un array), 
+stampare sulla pagina (anche brutalmente, basta che si vedano) 
+gli elementi della lista individualmente con un ciclo while.
+Consigli:
+- Ricordiamoci di inizializzare la variabile di contatore
+ prima dell'inizio del ciclo while
+- Ricordiamoci di incrementare il contatore, o comunque
+ di inserire una condizione di uscita, all'interno ciclo while
+*/
+
+
+
+
+
+
+//creo un array che contiene gli elementi della lista della spesa
+const listaSpesa = ["pane", "pasta", "acqua", "latte","uova"]
+console.log(listaSpesa)
+
+
+
